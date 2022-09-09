7ec947aa0a7836ce05bf8ba3b4954573c2047ed0
javarushgit <javarushgit@gmail.com>
2022-09-09 13:51:51 +0300

    init commit

---
+++ src/test/java/org/example/MyListTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MyListTest {
+  @Test
+  public void test() {
+    Assertions.assertTrue(true);
+  }
+}
