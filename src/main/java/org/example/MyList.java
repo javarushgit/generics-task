8529cadf9bc4870ebaa211a269645081bb31d031
UBCh <u2b127@gmail.com>
2022-09-11 20:42:41 +0300

    first save

--- src/main/java/org/example/MyList.java
+++ src/main/java/org/example/MyList.java
@@ -1,29 +1,115 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.function.Function;
+import java.util.stream.Stream;
 
-public class MyList {
-  public void add(Object o) {
-    throw new RuntimeException("Not implemented");
-  }
+public class MyList<T extends Number> implements Iterable<T> {
+    private T[] myList;
+    private int myListSize;
 
-  public Object get(int index) {
-    throw new RuntimeException("Not implemented");
-  }
+    public MyList(T... o) {
+        this.myListSize = o.length;
+        Object [] tmp=  new Object[o.length] ;
+            this.myList = (T[]) tmp; 
+            for (int i = 0; i < myListSize; i++) {
+            add(o);
+        }
 
-  private void resize() {
-    throw new RuntimeException("Not implemented");
-  }
+    }
 
-  public Object remove(int index) {
-    throw new RuntimeException("Not implemented");
-  }
+    public MyList() {
+        this.myListSize = 16;
+        this.myList = (T[]) new Object[myListSize];
+    }
 
-  public MyList map(Function f) {
-    throw new RuntimeException("Not implemented");
-  }
+    public void add(Object o) {
+        int count = myList.length;
+        if (count < 16) {
+            myList[count + 1] = (T) o;
+        } else {
+            resize();
+            myList[count + 1] = (T) o;
+        }
+        throw new RuntimeException("Not implemented");
+    }
 
-  public int size() {
-    throw new RuntimeException("Not implemented");
-  }
+    public Object get(int index) {
+        if (myList.length >= index) {
+            return myList[index];
+        }
+
+        throw new RuntimeException("Not implemented");
+    }
+
+    private void resize() {
+        int count = myList.length % 16;
+        T[] tmp = Arrays.copyOf(myList, count + 16);
+        myList = tmp;
+        throw new RuntimeException("Not implemented");
+    }
+
+    public void remove(int index) {
+        if (myList.length < index || index < 0) {
+            throw new RuntimeException("Not implemented");
+        }
+        T[] tmp = (T[]) new Object[myList.length - 1];
+        for (T znak : myList) {
+            int i = 0;
+            int j = 0;
+            if (znak != myList[index]) {
+                tmp[i] = myList[j];
+            } else {
+                j = j + 1;
+                continue;
+            }
+            i++;
+            j++;
+        }
+    }
+
+    public Object[] map(Function f) {
+        if (myList == null)  {throw new RuntimeException("Not implemented");}
+        Stream<T> stream=Stream.of(myList);
+        stream.map(x->f.apply(x));
+        return stream.toArray();
+    }
+
+    public int size() {
+        if (myList == null) {
+            throw new RuntimeException("Not implemented");
+        }
+        int count = 0;
+        for (T znak : myList) {
+            count++;
+        }
+        return count;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        Iterator<T> it = new Iterator<T>() {
+
+            private int currentIndex = 0;
+
+            @Override
+            public boolean hasNext() {
+                return currentIndex < myListSize && myList[currentIndex] != null;
+            }
+
+            @Override
+            public T next() {
+                return myList[currentIndex++];
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+        return it;
+    }
+
+
 }
--- src/test/java/org/example/MyListTest.java
+++ src/test/java/org/example/MyListTest.java
@@ -6,6 +6,10 @@ import org.junit.jupiter.api.Test;
 public class MyListTest {
   @Test
   public void test() {
-    Assertions.assertTrue(true);
+    MyList<Integer> test=new MyList<Integer>(1,2,3);
+    int expected=3;
+    int actual=test.size();
+    Assertions.assertEquals(expected,actual);
+
   }
 }
