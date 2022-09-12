aa199c2c02fd82cfe74ed4474350e799c86c9953
Vladimir Maslovskij <maslovskij.98@mail.ru>
2022-09-12 10:30:51 +0300

    The task is ready, but points 7,8 - while not

--- src/main/java/org/example/MyList.java
+++ src/main/java/org/example/MyList.java
@@ -1,29 +1,110 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-public class MyList {
-  public void add(Object o) {
-    throw new RuntimeException("Not implemented");
+/**
+ * Реализация списка MyList для численных типов
+ * @param <E>
+ * @author maslovskij.98
+ * @version 0.0.1
+ */
+public class MyList<E extends Number> {
+  private final int arraySize = 10;
+  private E[] array;
+
+  private int size = 0;
+
+  public MyList() {
+    this.array = (E[])(new Number[arraySize]);
   }
 
-  public Object get(int index) {
-    throw new RuntimeException("Not implemented");
+  public MyIterator iterator() {
+    return new MyIterator(this);
   }
 
-  private void resize() {
-    throw new RuntimeException("Not implemented");
+  public static void main(String[] args) {
+    MyList<Integer> list = new MyList<>();
+    list.add(1);
+    list.add(2);
+    MyList<Double> list1 = list.map(s -> (double) s);
+    System.out.println(list1.get(0));
+    System.out.println(list1.get(1));
+    Object s = null;
+    System.out.println(list.toString());
+  }
+  public void add(E o) {
+    if (size == arraySize - 1)
+      resize(arraySize * 2);
+    array[size++] = o;
   }
 
-  public Object remove(int index) {
-    throw new RuntimeException("Not implemented");
+  public E get(int index) {
+    return (E)array[index];
   }
 
-  public MyList map(Function f) {
-    throw new RuntimeException("Not implemented");
+  private void resize(int i) {
+    E[] newArray = Arrays.copyOf(array, i);
+    array = newArray;
+  }
+
+  public E remove(int index) {
+    Object removed = array[index];
+    for(int i = index; i <= size; i++)
+      array[i] = array[i + 1];
+      array[size--] = null;
+      if (size <= arraySize/2 && size > 10)
+        resize(arraySize/2);
+    return (E)removed;
+  }
+  public <R extends Number> MyList<R> map(Function<? super E, ? extends R> function) {
+    MyList<R> list = new MyList<>();
+    for (int i = 0; i < this.size; i++) {
+      list.add(function.apply(this.array[i]));
+    }
+    return list;
   }
 
   public int size() {
-    throw new RuntimeException("Not implemented");
+    return size;
+  }
+  public int MyHashCode() {
+
+  }
+
+  /**
+   * Разница между статическим вложенным классом и не статическим заключается в :
+   * 1. Объект не статического вложенного класса невозможно создать без объекта внешнего класса
+   * в отличие от статического.
+   * 2. Статический вложенный класс имеет доступ только к статическим полям внешнего класса.
+   * 3. Для создания объекта статического вложенного класса без объекта внешнего, нужно также
+   * указать имя внешнего класса, а затем вложенного через "." (напрмер :
+   *                                      MyList.MyIterator iterator = new MyList.MyIterator();
+   */
+  public class MyIterator implements Iterator {
+
+    private MyList list;
+    private int listSize;
+    public MyIterator(MyList list) {
+      this.list = list;
+      this.listSize = list.size;
+    }
+    public boolean hasNext() {
+      if (listSize == 0)
+        return false;
+      else return true;
+    }
+    public E next() {
+      if (listSize <= 0) {
+        return null;
+      } else {
+        return (E) list.get(list.size() - listSize--);
+      }
+    }
   }
 }
--- src/test/java/org/example/MyListTest.java
+++ src/test/java/org/example/MyListTest.java
@@ -3,9 +3,83 @@ package org.example;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Iterator;
+
 public class MyListTest {
   @Test
-  public void test() {
-    Assertions.assertTrue(true);
+  public void createListTest() {
+    MyList<Integer> list = new MyList<>();
+    MyList<Double> list1 = new MyList<>();
+    MyList<Short> list2 = new MyList<>();
+    MyList<Long> list3 = new MyList<>();
+    MyList<Byte> list4 = new MyList<>();
+    MyList<Float> list5 = new MyList<>();
+  }
+  @Test
+  public void listAddTest() {
+    MyList<Integer> list = new MyList<>();
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    int expectedSize = 3;
+    int actualSize = list.size();
+    Assertions.assertEquals(expectedSize, actualSize);
+  }
+  @Test
+  public void listGetTest() {
+    MyList<Integer> list = new MyList<>();
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    int expectedNum = 2;
+    int actualNum = list.get(1);
+    Assertions.assertEquals(expectedNum, actualNum);
+  }
+  @Test
+  public void listRemoveTest() {
+    MyList<Integer> list = new MyList<>();
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    list.remove(0);
+    int expectedSize = 2;
+    int actualSize = list.size();
+    int expectedNum = 2;
+    int actualNum = list.get(0);
+    Assertions.assertEquals(expectedSize, actualSize);
+    Assertions.assertEquals(expectedNum, actualNum);
+  }
+  @Test
+  public void changeListTypeTest() {
+    MyList<Integer> integerMyList = new MyList<>();
+    integerMyList.add(1);
+    integerMyList.add(20);
+    integerMyList.add(300);
+    MyList<Double> doubleMyList = integerMyList.map(s -> (double) s);
+    MyList<Float> floatMyList = integerMyList.map(s -> (float) s);
+    MyList<Long> longMyList = integerMyList.map(s -> (long) s);
+    MyList<Double> doubleMyList1 = longMyList.map(s -> (double) s);
+    Double d = new Double(2.0);
+    Float f = new Float(3f);
+    Long l = new Long(250000000);
+    Assertions.assertInstanceOf(d.getClass(), doubleMyList.get(0));
+    Assertions.assertInstanceOf(f.getClass(), floatMyList.get(0));
+    Assertions.assertInstanceOf(l.getClass(), longMyList.get(0));
+    Assertions.assertInstanceOf(d.getClass(), doubleMyList1.get(0));
+  }
+  @Test
+  public void iteratorTest() {
+    MyList<Double> listD = new MyList<>();
+    MyList<Integer> list = new MyList<>();
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    list.add(5);
+    Iterator iterator = list.iterator();
+    Iterator iteratorD = listD.iterator();
+    Assertions.assertTrue(iterator.hasNext());
+    Assertions.assertFalse(iteratorD.hasNext());
+    Assertions.assertNotNull(iterator.next());
+    Assertions.assertNull(iteratorD.next());
   }
 }
