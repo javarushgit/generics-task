f6f08054b766c36ce3263aaf6ebf8f35e9ee0a8b
Vladimir Semkin <vnsemkin@gmail.com>
2022-09-13 21:41:07 +0300

    Custom List homeTask v.1

--- src/main/java/org/example/MyList.java
+++ src/main/java/org/example/MyList.java
@@ -1,22 +1,77 @@
 package org.example;
-
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.function.Function;
+/**
+ * MyList is the custom ArrayList class implementation standing for working
+ * with elements extends Number.
+ * This implementation has add() method for adding elements to the list,
+ * get() method for getting certain elements from a list.
+ * Remove method standing for removing elements from the list.
+ * @param <T> extends Number
+ * @author vnsemkin@gmail.com
+ * @version 1
+ */
+public class MyList<T extends Number> {
+  private T[] myArray;
+  private int myArraySize = 0;
 
-public class MyList {
-  public void add(Object o) {
-    throw new RuntimeException("Not implemented");
+  @SuppressWarnings("unchecked")
+  public MyList(int initialCapacity) {
+    if (initialCapacity > 0) {
+      this.myArray = (T[]) new Number[initialCapacity];
+    } else if (initialCapacity == 0) {
+      this.myArray = (T[]) new Number[]{};
+    } else {
+      throw new IllegalArgumentException("Illegal Capacity: " +
+              initialCapacity);
+    }
   }
 
-  public Object get(int index) {
-    throw new RuntimeException("Not implemented");
+  @SuppressWarnings("unchecked")
+  public MyList() {
+    int DEFAULT_CAPACITY = 10;
+    this.myArray = (T[]) new Number[DEFAULT_CAPACITY];
   }
 
-  private void resize() {
-    throw new RuntimeException("Not implemented");
+  public void add(T t) {
+    if (myArraySize >= myArray.length) {
+      myArray = resize(myArray, true);
+    }
+    myArray[myArraySize] = t;
+    myArraySize++;
   }
 
-  public Object remove(int index) {
-    throw new RuntimeException("Not implemented");
+  public T get(int index) {
+    return this.myArray[index];
+  }
+
+  @SuppressWarnings("unchecked")
+  private T[] resize(T[] oldMyArray, boolean isGrow) {
+    T[] newArray;
+    if(isGrow) {
+      newArray = (T[]) new Number[(int) (oldMyArray.length * 1.5)];
+      System.arraycopy(oldMyArray, 0, newArray, 0, oldMyArray.length);
+    }else{
+      newArray = (T[]) new Number[(int) (oldMyArray.length / 1.5)];
+      System.arraycopy(oldMyArray, 0, newArray, 0, (int)(oldMyArray.length/1.5));
+    }
+    return newArray;
+  }
+
+  @SuppressWarnings("unchecked")
+  public T remove(int index) {
+    T removeElement = null;
+    myArray = myArraySize < myArray.length/1.5 ? resize(myArray,false):myArray;
+    int size = myArray.length - 1;
+      if (index >= 0 && size > index) {
+        removeElement = myArray[index];
+        myArray[index] = null;
+        System.arraycopy(myArray, index + 1, myArray, index, size - (index + 1));
+        myArray[size - 1] = null;
+        myArraySize--;
+      }
+    return removeElement;
   }
 
   public MyList map(Function f) {
@@ -24,6 +79,67 @@ public class MyList {
   }
 
   public int size() {
-    throw new RuntimeException("Not implemented");
+    return myArraySize;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder stringBuilder = new StringBuilder();
+    Iterator it = this.iterator();
+    stringBuilder.append("[");
+      while (it.hasNext()) {
+        stringBuilder.append(it.next()).append(",");
+      }
+    stringBuilder.deleteCharAt(stringBuilder.lastIndexOf(",")).append("]");
+    return String.valueOf(stringBuilder);
+  }
+
+  public Iterator<T> iterator() {
+    return new MyArrayIterator();
+  }
+
+  public class MyArrayIterator implements Iterator<T> {
+
+    public int index;
+
+    public MyArrayIterator() {
+      index = 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+      return index < myArray.length && MyList.this.get(index) != null;
+    }
+
+    @Override
+    public T next() {
+      return (T) MyList.this.get(index++);
+    }
+
+    @Override
+    public void remove() {
+      if (index > 0) {
+        MyList.this.remove(index - 1);
+        index--;
+      }
+    }
+  }
+
+  @Override
+  @SuppressWarnings("unchecked")
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || o.getClass() != getClass()) {
+      return false;
+    }
+    MyList<T> myList = (MyList<T>) o;
+    return Arrays.equals(myArray,myList.myArray);
+  }
+
+  @Override
+  public int hashCode() {
+    return Arrays.hashCode(myArray);
   }
 }
--- src/test/java/org/example/MyListTest.java
+++ src/test/java/org/example/MyListTest.java
@@ -1,11 +1,244 @@
 package org.example;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MyListTest {
+
   @Test
-  public void test() {
-    Assertions.assertTrue(true);
+  public void defaultSizeCreationTest() {
+    //GIVEN
+    MyList myList = new MyList<>();
+    List<Number> list = new ArrayList<>();
+    int expectedEmptyListSize = list.size();
+    //WHEN
+    int actualListSize = myList.size();
+    //THEN
+    Assertions.assertEquals(expectedEmptyListSize, actualListSize);
+  }
+
+  @Test
+  public void initialSizeCreationTest() {
+    //GIVEN
+    MyList myList = new MyList<>(20);
+    List<Number> list = new ArrayList<>(20);
+    int expectedListSize = list.size();
+    //WHEN
+    int actualListSize = myList.size();
+    //THEN
+    Assertions.assertEquals(expectedListSize, actualListSize);
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void elementsAdditionTest() {
+    //GIVEN
+    MyList myList = new MyList<>();
+    myList.add(0);
+    myList.add(1);
+    myList.add(2);
+    myList.add(3);
+    myList.add(4);
+    myList.add(5);
+    myList.add(6);
+    myList.add(7);
+    myList.add(8);
+    myList.add(9);
+    int expectedListSize = 10;
+    int expectedFirstElement = 0;
+    int expectedLastElement = 9;
+    //WHEN
+    int actualListSize = myList.size();
+    int actualFirstElement = (int) myList.get(0);
+    int actualLastElement = (int) myList.get(9);
+    //THEN
+    Assertions.assertEquals(expectedListSize, actualListSize);
+    Assertions.assertEquals(expectedFirstElement, actualFirstElement);
+    Assertions.assertEquals(expectedLastElement, actualLastElement);
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void growSizeTest() {
+    //GIVEN
+    MyList myList = new MyList<>();
+    myList.add(0);
+    myList.add(1);
+    myList.add(2);
+    myList.add(3);
+    myList.add(4);
+    myList.add(5);
+    myList.add(6);
+    myList.add(7);
+    myList.add(8);
+    myList.add(9);
+    myList.add(10);
+    myList.add(11);
+    //WHEN
+    int expectedSize = 12;
+    int actualSize = myList.size();
+    int expectedFirstElement = 0;
+    int expectedLastElement = 11;
+    int actualFirstElement = (int) myList.get(0);
+    int actualLastElement = (int) myList.get(11);
+    //THEN
+    Assertions.assertEquals(actualSize, expectedSize);
+    Assertions.assertEquals(expectedFirstElement, actualFirstElement);
+    Assertions.assertEquals(expectedLastElement, actualLastElement);
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void removeElementTest() {
+    //GIVEN
+    MyList myList = new MyList<>();
+    myList.add(0);
+    myList.add(1);
+    myList.add(2);
+    myList.add(3);
+    myList.add(4);
+    myList.add(5);
+    myList.add(6);
+    myList.add(7);
+    myList.add(8);
+    myList.add(9);
+    myList.add(10);
+    myList.add(11);
+    //WHEN
+    myList.remove(0);
+    int expectedListSize = 11;
+    int actualListSize = myList.size();
+    int expectedFirstElementValue = 1;
+    int actualFirstElementValue = (int) myList.get(0);
+    int expectedLastElementValue = 11;
+    int actualLastElementValue = (int) myList.get(10);
+    //THEN
+    Assertions.assertEquals(expectedListSize, actualListSize);
+    Assertions.assertEquals(expectedFirstElementValue, actualFirstElementValue);
+    Assertions.assertEquals(expectedLastElementValue, actualLastElementValue);
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void shrinkSizeTest() {
+    //GIVEN
+    MyList myList = new MyList<>();
+    myList.add(0);
+    myList.add(1);
+    myList.add(2);
+    myList.add(3);
+    myList.add(4);
+    myList.add(5);
+    myList.add(6);
+    myList.add(7);
+    myList.add(8);
+    myList.add(9);
+    myList.add(10);
+    myList.add(11);
+    //WHEN
+    myList.remove(0);
+    myList.remove(1);
+    myList.remove(2);
+    myList.remove(3);
+    Exception exception = assertThrows(ArrayIndexOutOfBoundsException.class, () -> {
+      myList.get(11);
+    });
+    String expectedMessage = "Index 11 out of bounds for length 10";
+    String actualMessage = exception.getMessage();
+    //THEN
+    Assertions.assertTrue(actualMessage.contains(expectedMessage));
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void iteratorTest() {
+    //GIVEN
+    MyList myList = new MyList<>();
+    myList.add(0);
+    myList.add(1);
+    myList.add(2);
+    myList.add(3);
+    //WHEN
+    Iterator it = myList.iterator();
+    StringBuilder stringBuilder = new StringBuilder();
+    stringBuilder.append("[");
+    while (it.hasNext()) {
+      stringBuilder.append(it.next()).append(",");
+    }
+    stringBuilder.deleteCharAt(stringBuilder.lastIndexOf(",")).append("]");
+    String expected = "[0,1,2,3]";
+    String actual = String.valueOf(stringBuilder);
+    int expectedValue = 0;
+    int actualValue = (int) myList.iterator().next();
+    //THEN
+    Assertions.assertEquals(expected,actual);
+    Assertions.assertEquals(expectedValue,actualValue);
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void equalsTest(){
+    //GIVEN
+    MyList myList = new MyList<>();
+    MyList myList1 = new MyList<>();
+    MyList myList2 = new MyList<>();
+    MyList myList3 = null;
+    List<Integer> myList4 = Arrays.asList(0,1,2,3);
+    myList.add(0);
+    myList.add(1);
+    myList.add(2);
+    myList.add(3);
+    myList1.add(0);
+    myList1.add(1);
+    myList1.add(2);
+    myList1.add(3);
+    myList2.add(1);
+    myList2.add(1);
+    myList2.add(2);
+    myList2.add(3);
+    //WHEN
+    boolean expected = true;
+    boolean actualList1 = myList.equals(myList1);
+    boolean actualList2 = myList.equals(myList2);
+    boolean actualList3 = myList.equals(myList3);
+    boolean actualList4 = myList.equals(myList4);
+    //THEN
+    Assertions.assertEquals(expected, actualList1);
+    Assertions.assertNotEquals(expected, actualList2);
+    Assertions.assertNotEquals(expected, actualList3);
+    Assertions.assertNotEquals(expected, actualList4);
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void hashCodeTest() {
+    //GIVEN
+    MyList myList = new MyList<>();
+    MyList myList1 = new MyList<>();
+    MyList myList2 = new MyList<>();
+    myList.add(0);
+    myList.add(1);
+    myList.add(2);
+    myList.add(3);
+    myList1.add(0);
+    myList1.add(1);
+    myList1.add(2);
+    myList1.add(3);
+    myList2.add(1);
+    myList2.add(1);
+    myList2.add(2);
+    myList2.add(3);
+    //WHEN
+    int expectedHash = myList.hashCode();
+    int actualHashList1 = myList1.hashCode();
+    int actualHashList2 = myList2.hashCode();
+    //THEN
+    Assertions.assertEquals(expectedHash, actualHashList1);
+    Assertions.assertNotEquals(expectedHash, actualHashList2);
   }
 }
