ab7532b60f703380e0b68ebf7707da7354e6e341
mrdobriykot <evgkorin@yandex.ru>
2022-11-21 21:41:07 +0300

    created methods add and resize

--- src/main/java/org/example/MyList.java
+++ src/main/java/org/example/MyList.java
@@ -1,10 +1,33 @@
 package org.example;
 
+import java.util.Iterator;
 import java.util.function.Function;
 
-public class MyList {
-  public void add(Object o) {
-    throw new RuntimeException("Not implemented");
+/*
+ * Внутренийк класс
+ * Объект внутреннего класса может быть создан только внутри внешнего класса.
+ * У него есть доступ ко всем полям объекта.
+ *
+ * Внутренний статический класс
+ * Объект класса может быть создан без объекта внешнего класса. Например MyList.MyIterator()
+ * У него есть доступ только к статическим полям внешнего класса.
+ */
+
+public class MyList<T extends Number> implements Iterable<T> {
+  private Number[] array;
+  private int size;
+
+  public MyList() {
+    array = new Number[16];
+    size = 0;
+  }
+
+  public void add(T o) {
+    array[size] = o;
+    size++;
+    if (size == array.length) {
+      resize();
+    }
   }
 
   public Object get(int index) {
@@ -12,7 +35,9 @@ public class MyList {
   }
 
   private void resize() {
-    throw new RuntimeException("Not implemented");
+     Number[] array2 = new Number[array.length *2];
+     System.arraycopy(array, 0, array2, 0, size);
+     array = array2;
   }
 
   public Object remove(int index) {
@@ -26,4 +51,21 @@ public class MyList {
   public int size() {
     throw new RuntimeException("Not implemented");
   }
+
+  @Override
+  public Iterator<T> iterator() {
+    return null;
+  }
+
+   public class MyIterator implements Iterator<T> {
+     @Override
+     public boolean hasNext() {
+       return false;
+     }
+
+     @Override
+     public E next() {
+       return null;
+     }
+   }
 }
